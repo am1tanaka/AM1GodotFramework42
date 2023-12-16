30545c99133ef2dd1ee8d69aeb2e1e1d5efd1183
YuTanaka <YRK00337@nifty.com>
2023-12-16 23:13:25 +0900

    Update fade.gd

--- am1/framework/scripts/fade.gd
+++ am1/framework/scripts/fade.gd
@@ -3,14 +3,27 @@ extends Node
 
 ## Fade処理用クラス
 
+## シグナル
+signal _fade_out
+signal _fade_in
+
+## キャッシュ
 @onready var colorRect : ColorRect = $CanvasLayer/ColorRect
+@onready var tween = create_tween()
 
 ## 読み込みと同時に消す
 func _ready():
 	colorRect.color.a = 0
+	start_fade_out(1.0, test_start_fade_in)
+
+func start_fade_out(sec: float, done):
 	
-## テストコード
-func _process(delta):
-	pass
-	# start fade in
-	# start fade out
+	print("start_fade_out")
+	_fade_out.connect(done)
+
+func start_fade_in(sec: float, done):
+	print("start_fade_in")
+
+func test_start_fade_in():
+	print("test_start_fade_in")
+
