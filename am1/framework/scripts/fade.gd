f75438e3b04c54b26b2168b5448870d0ff0cc9e9
Yu Tanaka <githubam1@am1.jp>
2023-12-17 00:32:13 +0900

    close #2 フェード処理実装

--- am1/framework/scripts/fade.gd
+++ am1/framework/scripts/fade.gd
@@ -8,22 +8,42 @@ signal _fade_out
 signal _fade_in
 
 ## キャッシュ
-@onready var colorRect : ColorRect = $CanvasLayer/ColorRect
-@onready var tween = create_tween()
+@onready var color_rect : ColorRect = $CanvasLayer/ColorRect
 
 ## 読み込みと同時に消す
 func _ready():
-	colorRect.color.a = 0
-	start_fade_out(1.0, test_start_fade_in)
+	color_rect.color.a = 0
+	start_fade_out(color_rect.color, 1.0, test_start_fade_in)
 
-func start_fade_out(sec: float, done):
+## フェードアウト実行
+## color フェードの色
+## sec フェード秒数
+## done フェードアウト完了時に呼び出したい関数
+func start_fade_out(color: Color, sec: float, done):
+	color.a = 0
+	color_rect.color = color
+
+	var final_color = color
+	final_color.a = 1
 	
-	print("start_fade_out")
-	_fade_out.connect(done)
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(color_rect, "color", final_color, sec).finished.connect(done)
 
+## フェードイン実行
+## sec フェード秒数
+## done フェードイン完了時に呼び出したい関数
 func start_fade_in(sec: float, done):
-	print("start_fade_in")
+	var final_color = color_rect.color
+	final_color.a = 0
+	
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(color_rect, "color", final_color, sec).finished.connect(done)
 
+## 動作確認用メソッド
 func test_start_fade_in():
-	print("test_start_fade_in")
+	start_fade_in(1.0, test_fade_in_done)
 
+func test_fade_in_done():
+	print("フェードイン完了")
